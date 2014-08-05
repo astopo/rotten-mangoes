18e1b99988637078375d5d77afe940c72fde4c28
astopo <amy.soetopo@gmx.com>
2014-08-05 19:06:11 +0000

    helper methods on users and reviews

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -21,11 +21,15 @@ class Movie < ActiveRecord::Base
 
   validate :release_date_is_in_the_future
 
+  def review_average
+    reviews.sum(:rating_out_of_ten)/reviews.size if reviews.size > 0
+  end
+
   protected
 
   def release_date_is_in_the_future
     if release_date.present?
       errors.add(:release_date, "should probably be in the future") if release_date < Date.today
     end
-  end
+  end 
 end
