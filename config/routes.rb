cdc8631dff28e8fbd163fddd2fdcf9d8a0114530
astopo <amy.soetopo@gmx.com>
2014-08-05 18:17:37 +0000

    added log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :movies
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
  
 
   # The priority is based upon order of creation: first created -> highest priority.
