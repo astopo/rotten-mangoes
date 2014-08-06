d21b7eccea9a9f5931e6e9b14102cf4b3010fe28
astopo <amy.soetopo@gmx.com>
2014-08-06 21:50:22 +0000

    can edit users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -25,7 +25,21 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   def destroy
